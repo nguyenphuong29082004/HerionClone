7d3967ecff1e6f57f80a3777800c34af727f50b3
nguyenphuong29082004 <nguyenphuong29082004@gmail.com>
2025-06-24 18:54:08 +0700

    add: footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import HomepageLayout from './components/homepage/HomePageLayout'
 import DiscographyLayout from './components/discography/DiscographyLayout'
 import ArtistLayout from './components/artist/ArtistLayout'
 import ShopLayout from './components/shop/ShopLayout'
+import Footer from './components/footer/Footer'
 
 
 function App() {
 
   return (
     <>
-      <HomepageLayout/>
+      {/* <HomepageLayout/>
       <DiscographyLayout/>
-      <ArtistLayout/>
+      <ArtistLayout/> */}
       <ShopLayout/>
+
+      {/* <Footer/> */}
       
       
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="mt-[700px] relative">
+        <div className="bg-[url(/footer/footer.jpg)] w-screen h-screen absolute"></div>
+        <div className='absolute bg-[#78777710] h-screen w-screen top-0 left-0'></div>
+    </div>
+  )
+}
+
+export default Footer
